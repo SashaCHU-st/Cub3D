51da2d548c4fc0cf3945964ce0b5b717880821ad
Aleksandra Heinanen <aleksandra.heinanen@gmail.com>
2024-10-01 13:21:23 +0300

    sasha's try

---
+++ check_map.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_map.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aheinane <aheinane@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/23 10:12:50 by aheinane          #+#    #+#             */
+/*   Updated: 2024/10/01 11:52:53 by aheinane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+void	all_found(t_textures *textures)
+{
+	// printf("1 %d\n", textures->found_no);
+	// printf("2 %d\n", textures->found_so);
+	// printf("3 %d\n", textures->found_we);
+	// printf("4 %d\n", textures->found_ea);
+	// printf("5 %d\n", textures->found_f);
+	// printf("6 %d\n", textures->found_c);
+	//textures->found = textures->found_no+textures->found_so+textures->found_we+textures->found_ea+textures->found_c + textures->found_f;
+	if(textures->found_no == 1 && textures->found_so == 1 && textures->found_we == 1 && textures->found_we == 1
+		&& textures->found_ea == 1 && textures->found_f == 1 && textures->found_c == 1)
+		textures->found = textures->found_no+textures->found_so+textures->found_we+textures->found_ea+textures->found_c + textures->found_f;
+	//printf("HHHH %d\n", textures->found);
+}
+
+int	checking_map(t_textures *textures, char *line, int number)
+{
+	int i = 0;
+	(void)textures;
+	(void)number;
+	while (line[i])
+	{
+		if (line[i] != '1' && line[i] != '0' && line[i] != ' ' && line[i] != '\n' && line[i] != '\t' )//&& line[i] != '\t'?????
+		{
+			if (line[i] == 'N' || line[i] == 'S' || line[i] == 'E' || line[i] == 'W')
+			{
+				textures->play.x = i*64;
+				textures->play.y = number*64;
+				printf("I =>%d\n", i *64);
+				printf("number =>%d\n", number*64);
+				textures->player_found++;
+				if (textures->player_found > 1 || textures->player_found == 0)
+					error_fun(textures);
+			}
+			else
+			{
+		//		printf("HHHH\n");
+				error_fun(textures);
+			}
+		}
+		i++;
+	}
+	return (1);
+}
---
+++ color.c
@@ -0,0 +1,125 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   color.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aheinane <aheinane@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/18 12:42:30 by aheinane          #+#    #+#             */
+/*   Updated: 2024/10/01 11:23:29 by aheinane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+int	is_valid_number(const char *str)
+{
+	int i = 0;
+	while (str[i] == ' ')
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+		i++;
+	if (!ft_isdigit(str[i]))
+		return 0;
+	while (str[i])
+	{
+		if (!ft_isdigit(str[i]))
+			return 0;
+		i++;
+	}
+	
+	return 1;
+}
+
+int	parse_color_values(t_textures *textures,const char *color_string, int *values)
+{
+	char **colors;
+	int i;
+	int j = 0;
+	i = 0;
+	colors = ft_split(color_string, ',');
+	if (!colors)
+	{
+		//printf("to split the color string.\n");
+		return 0;
+	}
+	while (colors[i] != NULL && i < 3)
+	{
+		if (is_valid_number(colors[i]))
+		{
+			values[i] = ft_atoi(colors[i]);
+			if(values[i] > 255 || values[i] < 0)
+				error_fun(textures);
+		}
+		else
+		{
+			j = 0;
+			while (colors[j] != NULL)
+			{
+				free(colors[j]);
+				j++;
+			}
+			free(colors);
+			error_fun(textures);
+		}
+		i++;
+	}
+	j =0;
+	while (colors[j] != NULL)
+	{
+		free(colors[j]);
+		j++;
+	}
+	free(colors);
+	if (i == 3)
+		return 1;
+	else
+	{
+	//	printf("expected 3.\n");
+		error_fun(textures);
+		return 0;
+	}
+}
+
+void	parse_floor_color(const char *color_string, t_textures *textures, bool is_floor)
+{
+	int	values[3];
+
+	if (parse_color_values(textures, color_string, values))
+	{
+		if (is_floor)
+		{
+			textures->floor_r = values[0];
+			textures->floor_g = values[1];
+			textures->floor_b = values[2];
+		//	printf("floor color: R = %d, G = %d, B = %d\n", textures->floor_r, textures->floor_g, textures->floor_b);
+		}
+		else
+		{
+			textures->ceiling_r = values[0];
+			textures->ceiling_g = values[1];
+			textures->ceiling_b = values[2];
+		//	printf("ceiling color: R = %d, G = %d, B = %d\n", textures->ceiling_r, textures->ceiling_g, textures->ceiling_b);
+		}
+	}
+	else
+		error_fun(textures);
+}
+
+void	checking_color(t_textures *textures, char *line)
+{
+	if (ft_strncmp(line, "F", 1) == 0 && check_space(line[1]))
+	{
+		textures->found_f += 1;
+		textures->floor_color = ft_strdup(avoid_whitespace(line + 2));
+		//printf("FLOOR COLOR: %s\n", textures->floor_color);
+		parse_floor_color(textures->floor_color, textures, true);
+	}
+	else if (ft_strncmp(line, "C", 1) == 0 && check_space(line[1]))
+	{
+		textures->found_c += 1;
+		textures->ceiling_color = ft_strdup(avoid_whitespace(line + 2));
+		//printf("CEILING COLOR: %s\n", textures->ceiling_color);
+		parse_floor_color(textures->ceiling_color, textures, false);
+	}
+}
